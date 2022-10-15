b3d775ae3c55289b7c12d83ca85590c34a1797a0
Ryan <leehahnwoong@gmail.com>
2022-10-15 11:33:33 -0700

    [codingbat] warmup1

---
+++ Ryan/codingBat/warmup1/parrot_trouble.py
@@ -0,0 +1,10 @@
+"""
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
+We are in trouble if the parrot is talking and the hour is before 7 or after 20. 
+Return True if we are in trouble.
+"""
+def parrot_trouble(talking, hour):
+    if talking and (hour > 20 or hour <7):
+        return True
+    else:
+        return False
