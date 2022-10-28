fed81e78caaec03e9ce85a59b13a85b3a8f56ce0
Ryan <leehahnwoong@gmail.com>
2022-10-28 09:47:53 -0700

    [add] q8 answer

---
+++ Ryan/doItPython/ch3/q9.py
@@ -0,0 +1,25 @@
+# n, m = input().split()
+# n = int(n)#4
+# m = int(m)#2
+
+# dna = list(input())#gata
+# temp = list(input().split())#ignore
+
+# acgt_lst = [int(i) for i in temp]#[1,0,0,1]
+
+# lst = ["a","c","g","t"]
+# count = 0
+# flag = True
+
+# dna_counts = [dna.count(i) for i in lst] #[2,0,1,1]
+# toMultiply = []
+
+# for i in range(len(dna_counts)):
+#     diff = dna_counts[i] - acgt_lst[i]
+#     if diff < 0:
+#         flag = False
+
+# if flag:
+#     fillNum = m - sum(acgt_lst)
+
+print(int(3/2))
