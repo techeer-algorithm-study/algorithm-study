ec4ba7873c18e397b3237c860ada6f44cd3a3c3a
KoneJ <kbvv67890@gmail.com>
2022-11-13 17:48:19 +0900

    Ch4 문제 풀이 완료

---
+++ KoneJ/Ch4/16.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(input())
+A = []
+
+for i in range(N):
+    A.append((int(input()), i))
+
+Max = 0
+sorted_A = sorted(A)
+
+for i in range(N):
+    if Max < sorted_A[i][1] - i:
+        Max = sorted_A[i][1] - i
+
+print(Max+1)
