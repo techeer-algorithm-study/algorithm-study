450542cd06c18c4b6f760a4442702be77d18c4bf
Ray Lee <wooo519@naver.com>
2022-11-26 17:57:25 +0900

    32~41

---
+++ JeongWoo/doItPython/ch6/36.py
@@ -0,0 +1,18 @@
+ans = 0
+A = list(map(str,input().split(" ")))
+
+def mySum(i) :
+    sum = 0
+    temp = str(i).split("+")
+    for i in temp :
+        sum += int(i)
+    return sum
+
+for i in range(len(A)) :
+    temp = mySum(A[i])
+    if i == 0 :
+        ans += temp
+    else : 
+        ans -= temp
+
+print(ans)
