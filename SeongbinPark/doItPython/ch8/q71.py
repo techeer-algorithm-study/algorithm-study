f569abe5a7f03e765addefa5eed48e1861b33343
SeongbinPark <binni0515@gmail.com>
2022-12-26 12:58:36 +0900

    8주차

---
+++ SeongbinPark/doItPython/ch8/q71.py
@@ -0,0 +1,56 @@
+import sys
+input = sys.stdin.readline
+
+N,M,K=map(int, input().split())
+treeHeight=0
+length=N
+
+while length!=0:
+    length//=2
+    treeHeight+=1
+
+# 트리 생성
+treeSize = pow(2, treeHeight+1)
+leftNodeStartIndex=treeSize//2-1
+tree = [0]*(treeSize+1)
+
+# 트리 초기화
+for i in range(leftNodeStartIndex+1, leftNodeStartIndex+N+1):
+    tree[i]=int(input())
+
+def setTree(i):
+    while i!=1:
+        tree[i//2]+=tree[i]
+        i-=1
+
+setTree(treeSize-1)
+
+# 값 변경 메서드
+def changeVal(index, value):
+    diff= value-tree[index]
+    while index>0:
+        tree[index]=tree[index]+diff
+        index=index//2
+
+# 부분합 구하는 메서드
+def getSum(s, e):
+    partSum=0
+    while s<=e:
+        if s%2==1:
+            partSum+=tree[s]
+            s+=1
+        if e%2==0:
+            partSum+=tree[e]
+            e-=1
+        s=s//2
+        e=e//2
+    return partSum
+
+for _ in range(M+K):
+    question, s, e=map(int, input().split())
+    if question==1: # 값 수정
+        changeVal(leftNodeStartIndex+s, e)
+    elif question ==2: # 출력
+        s=s+leftNodeStartIndex
+        e=e+leftNodeStartIndex
+        print(getSum(s,e))
